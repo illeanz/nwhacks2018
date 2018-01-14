b02f73a4d02987c1963ceb6fda6d7e1bf08eb04b
Ren Wang <renwang435@gmail.com>
2018-01-13 23:04:33 -0800

    Util Tool for Updating Mongo from pd DataFrame

---
+++ PushToMongo/PushToMongo.py
@@ -0,0 +1,14 @@
+import pymongo
+import json
+import pandas as pd
+
+uri = 'mongodb://chickenlittle:butter@ds255797.mlab.com:55797/population_db'
+
+count = 0
+time_delay = 0.01
+client = pymongo.MongoClient(uri)
+db = client.get_database('population_db')
+
+people = pd.read_csv('People.csv', header=0, index_col=0)
+records = json.loads(people.T.to_json()).values()
+db.population_db.insert_many(records)
---
+++ PushToMongo/Query.py
@@ -0,0 +1,13 @@
+import pymongo
+
+uri = 'mongodb://chickenlittle:butter@ds255797.mlab.com:55797/population_db'
+
+client = pymongo.MongoClient(uri)
+data = client.get_database('population_db')
+data = data['population_db']
+cursor = data.find({})
+
+for document in cursor:
+    print(document)
+
+client.close
