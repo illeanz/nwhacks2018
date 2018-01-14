6599554f1299ddd7e2b1d521f8d847a671d269d6
Ren Wang <renwang435@gmail.com>
2018-01-14 01:14:23 -0800

    Update DB Query

--- PushToMongo/Query.py
+++ PushToMongo/Query.py
@@ -3,8 +3,8 @@ import pymongo
 uri = 'mongodb://chickenlittle:butter@ds255797.mlab.com:55797/population_db'
 
 client = pymongo.MongoClient(uri)
-data = client.get_database('population_db')
-data = data['population_db']
+db = client.get_database('population_db')
+data = db['population_db']
 cursor = data.find({})
 
 for document in cursor:
